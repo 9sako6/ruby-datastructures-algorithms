1dbb4e7ca8b6da2cbf2d78b2e846280d8a098258
9sako6 <31821663+9sako6@users.noreply.github.com>
2019-10-30 12:13:04 +0900

    improve priority queue performance

--- lib/priority_queue.rb
+++ lib/priority_queue.rb
@@ -2,29 +2,30 @@ class PriorityQueue
   def initialize(&comp)
     @heap = []
     @node_num = 0
-    if block_given?
-      @comp = comp
-    else
+    if !block_given?
       @comp = Proc.new { |x, y| x <= y }
+    else
+      @comp = comp
     end
   end
+
   attr_reader :heap
- 
+
   def push(x)
-    i = @node_num # self node number
+    current_node_num = @node_num
     @node_num += 1
-    while i > 0
-      par = (i - 1) / 2 # parent's node number
-      break if @comp.call(@heap[par], x) # 逆転してないなら抜ける
+    while current_node_num > 0
+      parent_node_num = (current_node_num - 1) / 2
+      break if @comp.call(@heap[parent_node_num], x) # 逆転してないなら抜ける
       # 親ノードを下ろして自分を上に
-      @heap[i] = @heap[par]
-      i = par
+      @heap[current_node_num] = @heap[parent_node_num]
+      current_node_num = parent_node_num
     end
-    @heap[i] = x
+    @heap[current_node_num] = x
   end
- 
+
   alias_method(:<<, :push)
- 
+
   def pop
     # 最(小|大)値
     ret = @heap[0]
@@ -33,24 +34,24 @@ class PriorityQueue
     x = @heap[@node_num]
     # 根から下ろしていく
     i = 0
-    while i * 2 + 1 < @node_num
+    while (a = i * 2 + 1) < @node_num
       # 子どうしを比較
-      a, b = i * 2 + 1, i * 2 + 2
+      b = a + 1
       a = b if b < @node_num && @comp.call(@heap[b], @heap[a])
       break unless @comp.call(@heap[a], x)
       @heap[i] = @heap[a]
       i = a
     end
     @heap[i] = x
-    ret
+    return ret
   end
- 
+
   def get
     @node_num == 0 ? nil : @heap[0]
   end
- 
+
   def empty?
-    get.nil?
+    return get.nil?
   end
 end
 
